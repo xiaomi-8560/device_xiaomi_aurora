185677240e556a6ebe9d67ae097571b91dfba6ee
Akash Srivastava <akashniki@gmail.com>
2024-09-23 00:48:53 +0200

    aurora: Add gapps, faceunlock and inherit vendor/extra

--- lineage_aurora.mk
+++ lineage_aurora.mk
@@ -10,6 +10,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from aurora device.
 $(call inherit-product, device/xiaomi/aurora/pineapple.mk)
 
+# Gapps
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+
+# Vendor extra
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 # Device identifier
 PRODUCT_DEVICE := aurora
 PRODUCT_NAME := lineage_aurora
